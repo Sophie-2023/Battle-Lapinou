64dad9a1fd87f724672e82a6d5c3d16f86898471
Sophie-2023 <sophie.siniavski@gmail.com>
2025-01-07 19:16:16 +0100

    Ajout déplacement des unités par le joueur sur la map

--- Assets/Scripts/MoveUnit.cs
+++ Assets/Scripts/MoveUnit.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 
 public class MoveUnit : MonoBehaviour
 {
-    [SerializeField] private Camera _camera;
+    private Camera _camera;
+    [SerializeField] private LayerMask layerMask;
+    [SerializeField] private GameObject unitShadow;
     void Start()
     {
-        
+        _camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
     }
 
     void Update()
@@ -20,12 +22,20 @@ public class MoveUnit : MonoBehaviour
 
     private void OnMouseDrag()
     {
+        unitShadow.SetActive(true);
         Vector3 mousePosition = Input.mousePosition;
         Ray ray = _camera.ScreenPointToRay(mousePosition);
-        if (Physics.Raycast(ray, out RaycastHit hit, float.MaxValue))
+        if (Physics.Raycast(ray, out RaycastHit hit, float.MaxValue, layerMask))
         {
             Vector3 point = hit.point;
+            point.y = 3f; // Quand on place une unité sur la map, l'unité est surrélevée par rapport à la map
             transform.position = point;
+            Debug.Log(point);
         }
+    }
+
+    private void OnMouseExit()
+    {
+        unitShadow.SetActive(false);
     }
 }
